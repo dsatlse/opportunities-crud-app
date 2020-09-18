2413d5a4563a2dca7e7f5786458956de5f5aa929
Chris Chia <chrischia06@gmail.com>
2020-09-18 12:09:41 +0100

    Adding scripts, test data, preliminary docs

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # opportunities
 A  CRUD web app to record opportunities (careers events, jobs, hackathons) posted in the society
+
+
+This is an example table, there are two: JOBS and EVENTS which store a list of jobs posting and relevant events, and their associated information
+
+![example table](docs/example_table.png)
+
+The ```jobs_script.py``` produces formatted output from the table (ideally it should support a SELECT WHERE DATE_POSTED > x query) which can be posted in Zulip / Newsletter
+
+![formatted output](docs/format_output.png)
+
+The next step currently is a link shortener service, which will allow us to log number of clicks. currently each link needs to be added manually, with the user specificying the namespace (ideally it should be a hash instead of something like "events1")
+
+![link shortener](docs/link_shortener.png)
+
+## Development
+
+Ideally the whole workflow should be integrated, an end user should be able to add a new events / job posting to the database via a front-end interface or API, the shortened link should be made automatically and there should be a ```/format_output``` method to print the unposted postings
---
+++ jobs_script.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+def format_jobs(x):
+	"""
+	Inputs: Takes in rows of the JOBS database
+	Outputs: Produces a human readable formatting of each job posting
+	"""
+
+	#formatted_str must contain name
+	formatted_str = f"🤖 {x[3]}"
+	#location
+	if str(x[4]) != "nan":
+		formatted_str += f" - {x[4]}"
+	#deadline
+	if str(x[5]) != "nan":
+		formatted_str += f" | {x[5]}"
+	#url - using link shortener
+	if str(x[2]) != "nan":
+		formatted_str += f"\nhttps://link.kszk.eu/jobs{x[0]}"
+	#description 
+	if str(x[6]) != "nan":
+		formatted_str += f"\n{x[6]}"
+	return formatted_str
+
+def format_events(x):
+	"""
+	Inputs: Takes in rows of the EVENTS Database
+	Outputs: Produces a human readable formatting of each job posting
+	"""
+	formatted_str = ""
+	if x[3] == "WORKSHOP" or x[3] == "TALK":
+		formatted_str += "💥 "
+	else:
+		formatted_str += "📈 "
+
+	formatted_str += f"{x[4]}"
+	if str(x[5]) != "nan" and str(x[5]) != "":
+		formatted_str += f" - {x[5]}"
+	if str(x[6]) != "nan" and str(x[6]) != "":
+		formatted_str += f"-{x[6]} "
+	if str(x[7]) != "nan" and str(x[7]) != "":
+		formatted_str += f"\n{x[7]}"
+	if str(x[8]) != "nan" and str(x[8]) != "":
+		formatted_str += f"\nNB: {x[8]}"
+	if str(x[2]) != "nan" and str(x[2]) != "":
+		formatted_str += f"\nhttps://link.kszk.eu/events{x[0]}"
+	
+	
+	return formatted_str
+
+if __name__ == '__main__':
+	#test for events
+	events_test_df = pd.read_csv("test_data/Opportunities post - EVENTS.csv")
+	events_test_df = events_test_df[~events_test_df['ID'].isna()]
+	events_test_df['ID']= events_test_df['ID'].astype(int)
+	print("\n\n".join([format_events(x) for x in events_test_df.values]))
+
+	#test for jobs
+	jobs_test_df = pd.read_csv("test_data/Opportunities post - JOBS.csv")
+	jobs_test_df = jobs_test_df[~jobs_test_df['ID'].isna()]
+	jobs_test_df['ID']= jobs_test_df['ID'].astype(int)
+	print("\n\n".join([format_jobs(x) for x in jobs_test_df.values]))
