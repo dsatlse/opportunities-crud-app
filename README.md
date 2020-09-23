c57c58acdc06352c91812a2128d4b1f62009adf2
Chris Chia <chrischia06@gmail.com>
2020-09-23 15:30:28 +0100

    Update jobs_script.py based on revised schema

--- README.md
+++ README.md
@@ -6,6 +6,23 @@ This is an example table, there are two: JOBS and EVENTS which store a list of j
 
 ![example table](docs/example_table.png)
 
+The current schema is as follows:
+
+```EVENTS``` table schema
+
+| ID | DATE_ADDED | URL | TYPE | ORGANISATION | NAME | EVENT_START | EVENT_END | DESCRIPTION | NOTES |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+
+
+```JOBS``` table schema
+
+| ID | DATE_ADDED | URL | COMPANY_NAME | POST_NAME | LEVEL | LOCATION | DEADLINE | DESCRIPTION |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- |
+
+
+
 The ```jobs_script.py``` produces formatted output from the table (ideally it should support a SELECT WHERE DATE_POSTED > x query) which can be posted in Zulip / Newsletter
 
 ![formatted output](docs/format_output.png)
@@ -15,5 +32,7 @@ The next step currently is a link shortener service, which will allow us to log 
 ![link shortener](docs/link_shortener.png)
 
 ## Development
+
+Currently the 'front-end' is a colab notebook: https://colab.research.google.com/drive/10wYKGJU_LMGbf2qCVHQjPQ-lr2Q0rUUC
 
 Ideally the whole workflow should be integrated, an end user should be able to add a new events / job posting to the database via a front-end interface or API, the shortened link should be made automatically and there should be a ```/format_output``` method to print the unposted postings
--- jobs_script.py
+++ jobs_script.py
@@ -1,63 +1,69 @@
 import numpy as np
 import pandas as pd
 
-def format_jobs(x):
-	"""
-	Inputs: Takes in rows of the JOBS database
-	Outputs: Produces a human readable formatting of each job posting
-	"""
+def set_header_from_first_row(df):
+  df2 = df.copy()
+  df2.columns = df2.iloc[0]
+  df2 = df2.iloc[1:]
+  return df2
 
-	#formatted_str must contain name
-	formatted_str = f"🤖 {x[3]} - {x[4]}"
-	#location
-	if str(x[5]) != "nan" and str(x[5]) != "":
-		formatted_str += f" - {x[5]}"
-	#deadline
-	if str(x[6]) != "nan" and str(x[6]) != "":
-		formatted_str += f" | {x[6]}"
-	#url - using link shortener
-	if str(x[2]) != "nan":
-		formatted_str += f"\nhttps://link.kszk.eu/jobs{x[0]}"
-	#description 
-	if str(x[7]) != "nan" and str(x[7]) != "":
-		formatted_str += f"\n{x[7]}"
-	return formatted_str
+def format_field(prepend: str, x : str):
+    x = ' '.join(x.split()) #strip whitespace
+    if str(x) != "nan" and str(x) != "":  #if not empty add
+        return prepend + x
+    return ""
 
-def format_events(x):
-	"""
-	Inputs: Takes in rows of the EVENTS Database
-	Outputs: Produces a human readable formatting of each job posting
-	"""
-	formatted_str = ""
-	if x[3] == "WORKSHOP" or x[3] == "TALK":
-		formatted_str += "💥 "
-	else:
-		formatted_str += "📈 "
+class JobFormatter:
+    @staticmethod
+    def format_jobs(x):
+        """
+        Inputs: Takes in rows of the JOBS Database
+        Outputs: Produces a human readable formatting of each job posting
+        """
+        formatted_str = f"🤖 {x[3]}" #organisation name, emoji
+        formatted_str += format_field(" - ", x[4]) #post name
+        formatted_str += format_field(" - ", x[5]) #level
+        formatted_str += format_field(" - ", x[6]) #location
+        formatted_str += format_field("- Deadline: ",x[7]) #deadline
+        formatted_str += format_field("\n",x[8]) #description
+        formatted_str += format_field("\nhttps://link.kszk.eu/jobs", x[0])
+        
+        return formatted_str
 
-	formatted_str += f"{x[4]}"
-	if str(x[5]) != "nan" and str(x[5]) != "":
-		formatted_str += f" - {x[5]}"
-	if str(x[6]) != "nan" and str(x[6]) != "":
-		formatted_str += f"-{x[6]} "
-	if str(x[7]) != "nan" and str(x[7]) != "":
-		formatted_str += f"\n{x[7]}"
-	if str(x[8]) != "nan" and str(x[8]) != "":
-		formatted_str += f"\nNB: {x[8]}"
-	if str(x[2]) != "nan" and str(x[2]) != "":
-		formatted_str += f"\nhttps://link.kszk.eu/events{x[0]}"
-	
-	
-	return formatted_str
+
+class EventFormatter:
+    def __init__(self):
+        self.events_emoji_map = {"WORKSHOP":u"💥", "TALK":u"💥", "HACKATHON": u"📈"}
+        return
+
+    def format_events(self, x):
+        """
+        Inputs: Takes in rows of the EVENTS Database
+        Outputs: Produces a human readable formatting of each job posting
+        """
+        formatted_str = events_emoji_map.get(x[3], u"💥") #emoji
+        formatted_str += format_field("", x[4]) #organisation
+        formatted_str += format_field(" - ", x[5]) #title
+        formatted_str += format_field(" - ", x[6]) #date start
+        if x[6][:14] == x[7][:14]:
+            formatted_str += format_field("-", x[7][15:]) #date end
+        else:
+            formatted_str += format_field("-", x[7])
+        formatted_str += format_field("\n",x[8]) #description
+        formatted_str += format_field("\nNB: ",x[9]) #notes
+        formatted_str += format_field("\nhttps://link.kszk.eu/events", x[0])
+        
+        return formatted_str
 
 if __name__ == '__main__':
-	#test for events
-	events_test_df = pd.read_csv("test_data/Opportunities post - EVENTS.csv")
-	events_test_df = events_test_df[~events_test_df['ID'].isna()]
-	events_test_df['ID']= events_test_df['ID'].astype(int)
-	print("\n\n".join([format_events(x) for x in events_test_df.values]))
+    #test for events
+    events_test_df = pd.read_csv("test_data/Opportunities post - EVENTS.csv")
+    events_test_df = events_test_df[~events_test_df['ID'].isna()]
+    events_test_df['ID']= events_test_df['ID'].astype(int)
+    print("\n\n".join([format_events(x) for x in events_test_df.values]))
 
-	#test for jobs
-	jobs_test_df = pd.read_csv("test_data/Opportunities post - JOBS.csv")
-	jobs_test_df = jobs_test_df[~jobs_test_df['ID'].isna()]
-	jobs_test_df['ID']= jobs_test_df['ID'].astype(int)
-	print("\n\n".join([format_jobs(x) for x in jobs_test_df.values]))
+    #test for jobs
+    jobs_test_df = pd.read_csv("test_data/Opportunities post - JOBS.csv")
+    jobs_test_df = jobs_test_df[~jobs_test_df['ID'].isna()]
+    jobs_test_df['ID']= jobs_test_df['ID'].astype(int)
+    print("\n\n".join([format_jobs(x) for x in jobs_test_df.values]))
